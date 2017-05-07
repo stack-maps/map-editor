45bb2b6771c2771b8ed96c416bfd8050415ac1c3
Jiacong Xu <jiacong.xu@foxmail.com>
2017-05-06 22:20:34 -0400

    Created skeleton UI for the editor.

---
+++ Assets/Editor/AutoSave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSave
+{
+  static AutoSave()
+  {
+    EditorApplication.playmodeStateChanged = () =>
+    {
+      if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+      {
+        if (EditorSceneManager.SaveOpenScenes()) {
+          Debug.Log("Auto-save success!");
+        }
+
+        AssetDatabase.SaveAssets();
+      }
+    };
+  }
+}
