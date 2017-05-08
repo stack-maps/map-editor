40533dcaafecbf26c5cd284c38aafedb12d70573
Jiacong Xu <jiacong.xu@foxmail.com>
2017-05-08 15:01:09 -0400

    Added tooltip system.

---
+++ Assets/Scripts/Shared/Components/StringTooltipView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Util {
+  public class StringTooltipView : MonoBehaviour, ITooltipDisplayer {
+    public Text tooltipLabel;
+
+    #region ITooltipDisplayer implementation
+
+    public void SetupTooltip(Dictionary<object, object> userInfo) {
+      tooltipLabel.text = userInfo["tooltip"] as string;
+    }
+
+    #endregion
+  }
+}
