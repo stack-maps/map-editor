045a4365a4f13268a2eec6f48fd997d940d3036e
Jiacong Xu <jiacong.xu@foxmail.com>
2017-05-08 03:03:21 -0400

    Imported Material UI and created UI for login.

---
+++ Assets/Scripts/Shared/Components/MaskChar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A simple script used to replace the default InputField password character
+/// from asterisk to whatever we want.
+/// </summary>
+public class MaskChar : MonoBehaviour {
+  public char customMaskChar = '●';
+
+  void Start() {
+    GetComponent<InputField>().asteriskChar = customMaskChar;
+  }
+}
