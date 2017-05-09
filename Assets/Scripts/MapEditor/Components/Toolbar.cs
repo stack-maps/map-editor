89046889c073841388297c186dc36eabeb9b8cc7
Jiacong Xu <jiacong.xu@foxmail.com>
2017-05-08 21:34:51 -0400

    Implemented zoom.

---
+++ Assets/Scripts/MapEditor/Components/Toolbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MaterialUI;
+
+/// <summary>
+/// Provides access to the toolbar and ensures only one tool is selected at a
+/// time.
+/// </summary>
+public class Toolbar : MonoBehaviour {
+  public MaterialButton cursorButton;
+  public MaterialButton aisleButton;
+  public MaterialButton aisleAreaButton;
+  public MaterialButton wallButton;
+  public MaterialButton landmarkButton;
+  public MaterialButton undoButton;
+  public MaterialButton redoButton;
+  public MaterialButton referenceImageButton;
+  public MaterialButton zoomButton;
+
+
+}
---
+++ Assets/Scripts/MapEditor/Controllers/EditAreaController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditAreaController : MonoBehaviour {
+  public RectTransform canvas;
+
+  public ScrollRect scrollRect;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  // These levels of zoom are arbitrary and can be changed any time.
+  readonly float[] zoomLevels = {0.25f, 0.33f, 0.5f, 0.75f, 1, 2, 4};
+
+  /// <summary>
+  /// Updates the zoom level of the canvas.
+  /// </summary>
+  /// <param name="zoomLevel">Zoom level.</param>
+  public void UpdateZoom(int zoomLevel) {
+    if (zoomLevel < zoomLevels.Length) {
+      canvas.localScale = new Vector3(zoomLevels[zoomLevel], zoomLevels[zoomLevel], 1);
+    } else if (zoomLevel == zoomLevels.Length) {
+      // We assume we want fit width here. Do some calculations.
+      // We want to scale the view so the canvas sits at 90% of the view.
+      float ratio = scrollRect.viewport.rect.width * 0.9f / canvas.rect.width;
+      canvas.localScale = new Vector3(ratio, ratio, 1);
+    } else {
+      // We assume we want fit window here. Do some calculations.
+      // We want to scale the view so the canvas sits at 90% of the view.
+      float ratio1 = scrollRect.viewport.rect.width * 0.9f / canvas.rect.width;
+      float ratio2 = scrollRect.viewport.rect.height * 0.9f / canvas.rect.height;
+      float ratio = Mathf.Min(ratio1, ratio2);
+      canvas.localScale = new Vector3(ratio, ratio, 1);
+    }
+  }
+
+}
